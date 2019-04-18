522bd10e79930a022c0e4bd9324754fc8441ac49
2577960943 <2577960943@qq.com>
2019-04-18 09:30:57 +0800

    Initial commit

---
+++ src/com/bdqn/dao/text.java
@@ -0,0 +1,10 @@
+package com.bdqn.dao;
+
+/**
+ * Created by Administrator on 2019/4/18 0018.
+ */
+public class text {
+    public static void main(String[] args) {
+        System.out.println("hello tow 次");
+    }
+}
