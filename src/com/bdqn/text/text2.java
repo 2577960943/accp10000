ceeb5f881e004bb12da970eeffaaacd23da38e9b
2577960943 <2577960943@qq.com>
2019-04-18 09:39:09 +0800

    text/text2

---
+++ src/com/bdqn/text/text2.java
@@ -0,0 +1,10 @@
+package com.bdqn.text;
+
+/**
+ * Created by Administrator on 2019/4/18 0018.
+ */
+public class text2 {
+    public static void main(String[] args) {
+        System.out.println("22");
+    }
+}
